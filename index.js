e231e5029fcb2ed42ef4835f1389eda26e33895c
dimapaloskin <dima@paloskin.me>
2017-03-10 16:15:12 +0300

    Check x-forwarded-proto header

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function(request) {
-  var secure = request.connection.encrypted
+  var secure = request.connection.encrypted || request.headers['x-forwarded-proto'] === 'https'
   return 'http' + (secure ? 's' : '') + '://' +
     request.headers.host +
     request.url
